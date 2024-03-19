a10980c041398edbfc4fe528d9d939eccdcdd256
Adam Shaw <arshaw@users.noreply.github.com>
2024-03-19 01:26:43 -0400

    Revert "dont minify"
    This reverts commit 7af02f840f755f9a752493a40a11f2023cc08750.

--- config.js
+++ config.js
@@ -98,9 +98,6 @@ export default defineConfig({
   },
 
   vite: {
-    build: {
-      minify: false,
-    },
     resolve: {
       alias: componentOverrides.map((componentPath) => ({
         find: new RegExp('^.*\\/' + escapeRegExp(componentPath) + '$'),
